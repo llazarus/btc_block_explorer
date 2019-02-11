c0bb1fa43716af36fe0bb1f23ef8ca9e60eab70b
Leland Lazarus <llazarus93@gmail.com>
2019-02-10 21:32:02 -0800

    setup native-base. changed set up for getting address and exchange info

--- btc-explorer/App.js
+++ btc-explorer/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { CreateStackNavigator } from 'react-navigation';
+import { StyleSheet, View, AsyncStorage } from 'react-native';
+import { Container } from 'native-base';
 
 import AddressList from './components/AddressList';
 import AddAddress from './components/AddAddress';
 import Settings from './components/Settings';
 
-const RootStack = createStackNavigator({
-  AddressList: AddressList,
-  AddAddress: AddAddress,
-  Settings: Settings
-});
-
 export default class App extends React.Component {
+  state = {
+    loading: true,
+    currencySymbol: '',
+    currency: {},
+    addresses: {}
+  }
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    // get stored currency or assign default value if none
+    let userCurrency = await AsyncStorage.getItem('currency') || 'USD';
+    // get stored addresses or assign default value if none
+    let userAddrs = await AsyncStorage.getItem('addresses') || '';
+
+    const currencyResponse = await fetch(`https://api.coindesk.com/v1/bpi/currentprice/${userCurrency}.json`);
+    const jsonCurrency = await currencyResponse.json();
+    this.setState({
+      currencySymbol: userCurrency,
+      currency: jsonCurrency
+    });
+
+    if (userAddrs === '') {
+      // do the things for people that don't have stored addresses!
+
+      // for test
+      const responseAddresses = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa;1Ez69SnzzmePmZX3WpEzMKTrcBF2gpNQ55;1XPTgDRhN8RFnzniWCddobD9iKZatrvH4');
+      const jsonAddresses = await responseAddresses.json();
+      this.setState({
+        addresses: jsonAddresses,
+        loading: false
+      });
+    } else {
+      // do the things for people that have stored addresses!
+      const addressString = userAddrs.join(';');
+      const responseAddresses = await fetch(`https://api.blockcypher.com/v1/btc/main/addrs/${addressString}`);
+      const jsonAddresses = await responseAddresses.json();
+      this.setState({
+        addresses: jsonAddresses,
+        loading: false
+      });
+    }
+  }
+
   render() {
+    let addresses = {...this.state.addresses};
+    let currency = {...this.state.currency};
     return (
-      <View style={styles.container}>
-        {/* <Text>Open up App.js to start working on your app!</Text> */}
-        <AddressList />
-      </View>
+      <Container>
+        <AddressList
+          addresses={addresses}
+          currency={currency}
+          currencySymbol={this.state.currencySymbol}
+        />
+      </Container>
     );
   }
 }
--- btc-explorer/components/AddressList.js
+++ btc-explorer/components/AddressList.js
@@ -1,81 +1,22 @@
 import React from 'react';
-import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class AddressList extends React.Component {
-  state = {
-    loading: true,
-    addresses: [''],
-    balances: ['']
-  }
-
-  componentWillMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    let userAddrs = await AsyncStorage.getItem('addresses') || '';
-    
-    if (userAddrs === '') {
-      // do the things for people that dont have a list of addresses!
-
-      // for test
-      const response = await fetch('https://api.blockcypher.com/v1/btc/main/addrs/1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa;1Ez69SnzzmePmZX3WpEzMKTrcBF2gpNQ55;1XPTgDRhN8RFnzniWCddobD9iKZatrvH4');
-      const jsonResponse = await response.json();
-      let addressArr = [];
-      let balancesArr = [];
-  
-      for (let i = 0; i < jsonResponse.length; i += 1) {
-        addressArr.push(jsonResponse[`${i}`]["address"]);
-        balancesArr.push(parseInt(jsonResponse[`${i}`]["final_balance"]));
-      }
-  
-      this.setState({ addresses: addressArr, balances: balancesArr, loading: false });
-      // 
-    } else {
-      // do the things for people that have a list of addresses!
-      let addressString = userAddrs.join(";");
-      const response = await fetch(`https://api.blockcypher.com/v1/btc/main/addrs/${addressString}`);
-      const jsonResponse = await response.json();
-      let addressArr = [];
-      let balancesArr = [];
-
-      for (let i = 0; i < jsonResponse.length; i += 1) {
-        addressArr.push(jsonResponse[`${i}`]["address"]);
-        balancesArr.push(parseInt(jsonResponse[`${i}`]["final_balance"]));
-      }
-  
-      this.setState({ addresses: addressArr, balances: balancesArr, loading: false });
-    }
-  }
-  
   render() {
-    const satoshiConversion = (sats) => { return sats/100000000 }
-    const sumBalance = (array) => {
-      let sumSats = array.reduce(function(accumulator, currentValue) {
-        return accumulator + currentValue;
-      });
-      return satoshiConversion(sumSats);
+    let rate = '';
+    if (this.props.currency.bpi) {
+      rate = this.props.currency.bpi.USD.rate_float;
     }
-    const listLength = this.state.addresses.length;
-    const listArr = [];
-    for (let i = 0; i < listLength; i += 1) {
-      listArr.push(i.toString());
+    let currencySymbol = '';
+    if (this.props.currencySymbol) {
+      currencySymbol = this.props.currencySymbol;
     }
-
+    
     return (
       <View style={styles.container}>
-        <Text>Hello from AddressList!</Text>
-        {listArr.map(addr => (
-          <View key={`view-${addr}`}>
-            <Text key={`addr-${addr}`}>{this.state.addresses[addr]}</Text>
-            <Text key={`balance-${addr}`}>{satoshiConversion(this.state.balances[addr])}</Text>
-          </View>
-        ))}
-        <View>
-          <Text>
-            Sum Balance: {sumBalance(this.state.balances)}
-          </Text>
-        </View>
+        <Text>
+          Hello from AddressList - {rate} {currencySymbol}
+        </Text>
       </View>
     );
   }
