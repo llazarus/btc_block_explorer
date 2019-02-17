616a11858a89322fd50dd68f53cc6d2b5bd7f7fb
Leland Lazarus <llazarus93@gmail.com>
2019-02-17 13:17:28 -0800

    rough draft Settings

--- btc-explorer/components/Settings.js
+++ btc-explorer/components/Settings.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Content, List, ListItem } from 'native-base';
 
 export default class Settings extends React.Component  {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello from Settings.js!</Text>
-      </View>
+      <Container>
+        <Content>
+          <List>
+            <ListItem itemDivider>
+              <Text>OPTIONS</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Currency</Text>
+            </ListItem>
+
+            <ListItem itemDivider>
+              <Text>SOCIAL</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Rate this app in the App Store</Text>
+            </ListItem>
+
+            <ListItem itemDivider>
+              <Text>BTC Block Explorer</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Feedback</Text>
+            </ListItem>
+            <ListItem>
+            <Text>Privacy Policy</Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
     );
   }
 }
